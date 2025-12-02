440fb8e1cbfbb7e9b578140de04fd41f2fed121e
Alexia Alves <lexajosielly@gmail.com>
2025-12-01 21:24:52 -0300

    feat: Adiciona comando concreto LightOnCommand, encapsulando a ação e o log.

---
+++ LightOnCommand.java
@@ -0,0 +1,19 @@
+public class LightOnCommand implements Command {
+    private Light light;
+    
+    public LightOnCommand(Light light) { 
+        this.light = light; 
+    }
+    
+    public void execute() { 
+        light.on(); 
+    }
+    
+    public void undo() { 
+        light.off(); 
+    }
+    
+    public String getLogData() { 
+        return "LIGHT_ON"; 
+    }
+}
